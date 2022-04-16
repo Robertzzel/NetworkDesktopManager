4a38d337a1be079e2a66e0959c3a36b7364f0083
Robertzzel <robertzel_2001@yahoo.com>
2022-04-16 19:40:00 +0300

    Culoare screenshot fixata

--- Screen/screenshot_tool.py
+++ Screen/screenshot_tool.py
@@ -1,38 +1,46 @@
 import cv2
 import pyautogui
 import numpy as np
+from pathlib import Path
 
 
 class ScreenshotTool:
+    CURSOR_PATH = f"{Path(__file__).parent.parent}\\Images\\cursor.jpg"
 
     def __init__(self):
-        screenshot = pyautogui.screenshot()
-        self._screen_shape = np.array(screenshot).shape
+        self._screen_shape = np.array(pyautogui.screenshot()).shape
 
-    def get_screenshot(self):
-        cursor_image = ScreenshotTool._get_cursor_image()
+    def get_screenshot(self) -> np.ndarray:
+        cursor_image = self.get_cursor_image()
         cursor_x, cursor_y = pyautogui.position()
-        screenshot_without_cursor = np.array(pyautogui.screenshot())
+        image = self.get_screenshot_image()
 
         try:
-            screenshot_without_cursor[cursor_y: cursor_y + cursor_image.shape[0],
-                                      cursor_x: cursor_x + cursor_image.shape[1]] = cursor_image
+            image[cursor_y: cursor_y + cursor_image.shape[0],
+                  cursor_x: cursor_x + cursor_image.shape[1]] = cursor_image
         except:
             pass
 
-        return screenshot_without_cursor
+        return image
 
-    @staticmethod
-    def _get_screenshot_without_cursor():
-        screenshot_without_cursor = np.array(pyautogui.screenshot())
-        return cv2.cvtColor(screenshot_without_cursor, cv2.COLOR_RGB2RGBA)
+    def get_screenshot_image(self):
+        rgb_image = pyautogui.screenshot()
+        bgr_image = cv2.cvtColor(np.array(rgb_image), cv2.COLOR_RGB2BGR)
+        return cv2.cvtColor(bgr_image, cv2.COLOR_BGR2BGRA)
 
-    @staticmethod
-    def _get_cursor_image():
-        cursor_image = cv2.imread("Images/cursor.png", cv2.IMREAD_UNCHANGED)
-        cursor_new_dimensions = (cursor_image.shape[0] // 10, cursor_image.shape[1] // 10)
-        cursor_image = cv2.resize(cursor_image, cursor_new_dimensions, interpolation=cv2.INTER_AREA)
-        return cursor_image
+    def get_cursor_image(self):
+        cursor_image = cv2.imread(self.CURSOR_PATH)
+        return cv2.cvtColor(cursor_image, cv2.COLOR_BGR2BGRA)
 
     def get_screen_shape(self):
         return self._screen_shape
+
+
+if __name__ == "__main__":
+    st = ScreenshotTool()
+    ss = st.get_screenshot()
+    ss2 = st.get_screenshot()
+    cv2.imshow("SS", ss)
+    cv2.waitKey(0)
+    cv2.imshow("SS", ss2)
+    cv2.waitKey(0)
--- run.py
+++ run.py
@@ -6,14 +6,13 @@ import time
 
 
 if __name__ == "__main__":
-    try:
-        receiver_process = Popen([executable, receiver.__file__])
-        time.sleep(1)
-        sender_process = Popen([executable, sender.__file__])
-        time.sleep(1)
-        input()
-    except:
-        pass
+
+    receiver_process = Popen([executable, receiver.__file__])
+    time.sleep(1)
+    sender_process = Popen([executable, sender.__file__])
+    time.sleep(1)
+    input()
+
 
     receiver_process.kill()
     sender_process.kill()

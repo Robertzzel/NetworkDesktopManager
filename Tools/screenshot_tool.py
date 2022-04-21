744a7b2a4b31fa61b8faf091e89f0504a63bbcb0
Robertzzel <robertzel_2001@yahoo.com>
2022-04-21 21:37:57 +0300

    Crescut viteza de captura pe windows

--- Tools/screenshot_tool.py
+++ Tools/screenshot_tool.py
@@ -1,18 +1,27 @@
-from cv2 import imread, cvtColor, COLOR_RGB2BGR, imshow, waitKey, IMREAD_COLOR, IMREAD_UNCHANGED, COLOR_RGB2BGRA
+from cv2 import imread, cvtColor, COLOR_BGRA2BGR, COLOR_RGB2BGR
 from pyautogui import screenshot
 from pyautogui import position
 from numpy import array, ndarray
 from configurations import Configurations
+from mss import mss
+from platform import platform
 
 
 class ScreenshotTool:
     def __init__(self):
         self._screen_shape = array(screenshot()).shape
         self.cursor_image: ndarray = imread(Configurations.CURSOR_IMAGE_PATH)
+        self._monitor = {
+            "top": 0,
+            "left": 0,
+            "width": self._screen_shape[1],
+            "height": self._screen_shape[0]
+        }
+        self._is_windows = platform().startswith("Windows")
 
     def get_screenshot(self) -> ndarray:
         cursor_x, cursor_y = position()
-        image = self.get_screenshot_image()
+        image = self._get_screenshot_image()
 
         if cursor_x > 0 and cursor_x + self.cursor_image.shape[1] < image.shape[1] \
                 and cursor_y > 0 and cursor_y + self.cursor_image.shape[0] < image.shape[0]:
@@ -25,18 +34,16 @@ class ScreenshotTool:
 
         return image
 
-    def get_screenshot_image(self):
-        rgb_image = screenshot()
-        bgr_image: ndarray = cvtColor(array(rgb_image), COLOR_RGB2BGR)
-        return bgr_image
+    def _get_screenshot_image(self):
+        if self._is_windows:
+            with mss() as sct:
+                return cvtColor(array(sct.grab(self._monitor)), COLOR_BGRA2BGR)
+        else:
+            return cvtColor(array(screenshot()), COLOR_RGB2BGR)
 
     def get_screen_shape(self):
         return self._screen_shape
 
 
 if __name__ == "__main__":
-    st = ScreenshotTool()
-    ss = st.get_screenshot()
-    print(ss.shape)
-    imshow("SS", ss)
-    waitKey(0)
+    pass
